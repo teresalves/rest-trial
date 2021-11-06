adccb328817ca789cf6637b3a90a84a868c16afe
teresalves <teresalves.mc@gmail.com>
2021-11-06 02:15:33 +0000

    feat: add endpoint to get all books in database

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
 FROM node:17.0.1-alpine3.14
 
 COPY package.json tsconfig.json /build/
-COPY ./src /build/src
 
 WORKDIR /build
 
 RUN yarn install
 
-RUN ls
+COPY ./src /build/src
 
 ENTRYPOINT ["yarn", "dev"]
--- src/app.ts
+++ src/app.ts
@@ -3,89 +3,33 @@ import { Client, Pool } from 'pg';
 const app = express();
 const port = 3000;
 
+const clientConfig = {
+  host: process.env.PGHOST || 'localhost',
+  user: process.env.PGUSER || 'teresalves',
+  password: process.env.PGPASSWORD || 'example',
+  port: Number(process.env.PGPORT) || 3300,
+  database: process.env.PGDATABASE || 'jorgedb',
+};
 
-// const client = new Client();
-// client.connect().catch((error: any) => {
-//   console.log('erro connecting')
-//   console.log(error);
-// });
-
-let client;
-
-
-try {
-  let pgDbConfig;
-  pgDbConfig = {
-    host: process.env.PGHOST || '',
-    user: process.env.PGUSER || '',
-    password: process.env.PGPASSWORD || '',
-    port: Number(process.env.PGPORT) || 8080,
-    database: process.env.PGDATABASE || '',
-  };
-  console.log(pgDbConfig);
-
-  const pgPool = new Pool(pgDbConfig);
-
-  // the pool will emit an error on behalf of any idle clients it contains if a backend error or network partition happens
-  pgPool.on('error', (err, client) => {
-    console.error('Unexpected error on idle pg client', err, client);
-  });
-
-  // pgPool.connect((err, client, release) => {
-  //   if (err) {
-  //     return console.error('Error acquiring client', err.stack)
-  //   }
-  //   client.query('SELECT NOW()', (err, result) => {
-  //     release()
-  //     if (err) {
-  //       return console.error('Error executing query', err.stack)
-  //     }
-  //     console.log(result.rows)
-  //   })
-  // })
-  // trying to use error listener after the connect
-  pgPool.on('connect', () => {
-    pgPool.on('error', err => console.log(err));
-  });
-
-  // pgPool.query('SELECT NOW()', (err, res) => {
-  //   console.log(err, res)
-  //   pgPool.end()
-  // })
-
-  pgPool.query('SELECT * FROM books', (err, res) => {
-    console.log(res.rows);
-    pgPool.end();
-  })
-  console.log('API is ready');
-  // client = connectToPool(pgPool);
-
-} catch(exception) {
-  console.error('API is not ready');
-  process.exit(1);
-}
-
-const query = `
-SELECT * FROM books;
-`;
-
-/* client.query(query, (err: any, res: any) => {
-  if (err) {
-      console.error(err);
-      return;
-  }
-  console.log(res.rows);
-  client.end();
-}); */
+console.log(clientConfig)
+const client = new Client(clientConfig);
+client.connect().catch((error: any) => {
+  console.log('erro connecting')
+  console.log(error);
+});
 
 
 app.get('/', (req: any, res: any) => {
-  res.send('Hello World!\n');
-  console.log("Grelos");
+  res.send('Hi there \n');
 })
 
+app.get('/allbooks', async (req: any, res: any) => {
+  const result = await client.query('SELECT * FROM books');
+  res.send(result.rows);
+});
 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
+
